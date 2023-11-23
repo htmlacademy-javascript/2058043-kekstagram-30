6601444a96ce0e27bbee5d10d27be90b1d947e1d
Keks <keks@htmlacademy.ru>
2023-11-23 09:30:23 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/util.js
@@ -0,0 +1,9 @@
+//случайное целое
+const getRandomNumber = (left, right) => Math.trunc(Math.random() * (right + 1 - left) + left);
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomArrayElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomNumber, getRandomArrayElement,getRandomInteger, isEscapeKey};
