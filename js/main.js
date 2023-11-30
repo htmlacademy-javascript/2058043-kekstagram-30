88c6ed7f6ee2dc3f9990b4fd50059c87fc5a9752
admin <ga@usr.by>
2023-11-30 22:54:40 +0300

    30112023 итоговые исправления

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { showDataErrorMessage } from './util.js';
 import { initFilter } from './filter.js';
 import { setupForm } from './form.js';
 setupForm();
+
 const loadPhotos = (data) => {
   renderPhotos(data);
   initFilter(data);
